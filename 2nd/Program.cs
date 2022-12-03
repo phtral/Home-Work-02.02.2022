e0807f377e0ccdb49bfa1b139d0fa262593d0d3a
Philip Trubitsyn <trph@bk.ru>
2022-12-03 10:48:17 +0300

    вторая задача из домашнего задания

---
+++ 2nd/Program.cs
@@ -0,0 +1,27 @@
+﻿Random rnd = new Random();
+int number = rnd.Next(1, 10000000);
+
+int digitCount = (int)Math.Log10(number) + 1;
+
+Console.WriteLine(number);
+
+if(digitCount >=3)
+{
+
+while ( digitCount > 3 )
+{
+   number = number / 10;
+   digitCount = digitCount - 1;
+}
+
+ int result = number % 10;
+ Console.WriteLine($"Третья цифра {result}");
+}
+
+else{
+
+  Console.WriteLine("Третьей цифры нет");
+}
+
+
+
