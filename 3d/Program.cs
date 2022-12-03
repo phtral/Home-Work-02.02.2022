53e17fee18011234ac50c6b44110d37bd987c43b
Philip Trubitsyn <trph@bk.ru>
2022-12-03 21:44:53 +0300

    Третья задача из домашнего задания

---
+++ 3d/Program.cs
@@ -0,0 +1,16 @@
+﻿Random rnd = new Random();
+int number = rnd.Next(1, 8);
+
+Console.WriteLine(number);
+
+if (number == 6 || number == 7 )
+{
+Console.WriteLine("Да");
+}
+else{
+Console.WriteLine("Нет");
+}
+
+
+
+ 
